808f8a6915940e65ba9501f40f483746ad487c55
asinha52 <ankitasinhaa444@gmail.com>
2026-01-12 19:57:35 -0600

    Add files via upload

---
+++ Fashion Inventory Analyzer pt.2.py
@@ -0,0 +1,12 @@
+
+
+
+prices = [120, 45, 250, 80, 300, 60, 150]
+
+for i in range(len(prices)):
+    if prices[i] > 100:
+        old_price = prices[i]
+        prices[i] = prices[i] * 0.8
+        print("Premium item:", old_price, "->", prices[i])
+
+print("Final prices:", prices)
